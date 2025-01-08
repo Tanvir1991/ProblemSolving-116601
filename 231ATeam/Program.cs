de0c15ce17b77ba77fce1e96b1f2ac28414fbd7d
Tanvir Ahmmed <tanvirahmmed194@gmail.com>
2025-01-08 17:20:44 +0600

    231A Problem solving

---
+++ 231ATeam/Program.cs
@@ -0,0 +1,108 @@
+﻿
+
+
+int n = int.Parse(Console.ReadLine());
+
+int count = 0;
+
+for (int i = 0; i < n; i++)
+{
+    string[] input = Console.ReadLine().Split();
+    int patya = int.Parse(input[0]);
+    int vasya = int.Parse(input[1]);
+    int tonya = int.Parse(input[2]);
+
+    if (patya + vasya + tonya >= 2)
+    {
+        count++;
+
+    }
+
+
+}
+Console.WriteLine(count);
+
+
+
+//int n = int.Parse(Console.ReadLine());
+
+//var count = Enumerable.Range(0, n).Select(_ => Console.ReadLine().Split(' ').Select(int.Parse).Sum()).Count(sum => sum >= 2);
+
+//Console.WriteLine(count);
+
+
+//int n = int.Parse(Console.ReadLine());
+
+//int count = 0;
+//for (int i = 0; i < n; i++)
+//{
+
+//    if (Array.ConvertAll(Console.ReadLine().Split(), int.Parse).Sum() >= 2)
+//    {
+//        count++;
+
+//    }
+
+//}
+//Console.WriteLine(count);
+
+//int n = int.Parse(Console.ReadLine()); // Read the number of problems
+//int count = 0;
+
+//for (int i = 0; i < n; i++)
+//{
+
+//    if (Array.ConvertAll(Console.ReadLine().Split(), int.Parse).Sum() >= 2)
+//    {
+//        count++;
+//    }
+//}
+
+//Console.WriteLine(count); // Print the result
+
+
+
+
+
+
+
+
+//// Read the number of problems
+//int n = int.Parse(Console.ReadLine());
+
+//// Use LINQ to read and process the input, counting valid problems
+//int count = Enumerable.Range(0, n)
+//    .Select(_ => Console.ReadLine().Split(' ').Select(int.Parse).Sum())
+//    .Count(sum => sum >= 2);
+
+//int test = Enumerable.Range(0, n).Select(_ => Console.ReadLine().Split(' ').Select(int.Parse).Sum()).Count(sum => sum >= 2);
+
+//// Output the result
+//Console.WriteLine(count);
+
+
+
+
+
+
+//// Read the number of problems
+//int n = int.Parse(Console.ReadLine());
+//int count = 0;
+
+//for (int i = 0; i < n; i++)
+//{
+//    // Read the opinions for the current problem
+//    string[] input = Console.ReadLine().Split(' ');
+//    int petya = int.Parse(input[0]);
+//    int vasya = int.Parse(input[1]);
+//    int tonya = int.Parse(input[2]);
+
+//    // Check if at least two friends are confident
+//    if (petya + vasya + tonya >= 2)
+//    {
+//        count++;
+//    }
+//}
+
+//// Output the count of problems they will implement
+//Console.WriteLine(count);
---
+++ WayTooLongWords/Program.cs
@@ -0,0 +1,33 @@
+﻿
+
+
+
+
+using System.Text;
+
+int n=int.Parse(Console.ReadLine());
+var listofWord=new List<string>();
+
+//var listeee = new StringBuilder();
+
+for (int i = 0; i < n; i++)
+{
+    string text = Console.ReadLine();
+    listofWord.Add(text);
+   // listeee.Append(text);
+}
+
+foreach (string word in listofWord)
+{
+    if (word.Length > 10)
+    {
+        string avreationword = $"{word[0]}{word.Length - 2}{word[^1]}";
+        Console.WriteLine(avreationword);
+
+    }
+    else
+    {
+        Console.WriteLine(word);
+    }
+}
+
